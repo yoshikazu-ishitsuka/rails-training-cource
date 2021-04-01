8d0a670df5747c31b75109625eeb6ef2ba4cb536
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-04-01 16:52:55 +0900

    掲示板データの作成

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -6,4 +6,14 @@ class BoardsController < ApplicationController
     @board = Board.new
   end
 
+  def create
+    Board.create(board_params)
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:name, :title, :body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # root 'boards#index'
   get 'boards', to: 'boards#index'
   get 'boards/new', to: 'boards#new'
+  post 'boards', to: 'boards#create'
 end
