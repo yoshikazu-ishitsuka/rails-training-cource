beb818913e939079c571f078742853e22840f3a2
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-04-03 10:53:46 +0900

    コントローラーのフィルター機能

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,6 @@
 class BoardsController < ApplicationController
+  before_action :set_target_board, only: %i[show edit update destroy]
+
   def index
     @boards = Board.all
   end
@@ -13,23 +15,19 @@ class BoardsController < ApplicationController
   end
 
   def show
-    @board = Board.find(params[:id])
   end
 
   def edit
-    @board = Board.find(params[:id])
   end
 
   def update
-    board = Board.find(params[:id])
-    board.update(board_params)
+    @board.update(board_params)
 
-    redirect_to board
+    redirect_to @board
   end
 
   def destroy
-    board = Board.find(params[:id])
-    board.delete
+    @board.delete
 
     redirect_to boards_path
   end
@@ -41,4 +39,7 @@ class BoardsController < ApplicationController
     params.require(:board).permit(:name, :title, :body)
   end
 
+  def set_target_board
+    @board = Board.find(params[:id])
+  end
 end
