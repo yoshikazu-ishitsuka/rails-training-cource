f3ef308a6752edd2ce3fc20b8dce20e52351a836
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-04-05 10:51:54 +0900

    commentsコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'comments/create'
+
+  get 'comments/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'boards#index'
   # get 'boards', to: 'boards#index'
