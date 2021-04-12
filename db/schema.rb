c67a9fac0604f43fd86ee6330638186c553e3606
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-04-12 16:40:26 +0900

    RSpecの構文について

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_08_025608) do
+ActiveRecord::Schema.define(version: 2021_04_12_071939) do
 
   create_table "board_tag_relations", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "board_id"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2021_04_08_025608) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "birthday"
     t.index ["name"], name: "index_users_on_name", unique: true
   end
 
