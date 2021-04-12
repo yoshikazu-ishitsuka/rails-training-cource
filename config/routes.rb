10ce1b2cb1a4bcb65ef92cfb5e75bf2c58441274
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-04-12 12:07:55 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/me'
-
-  get 'home/index'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
+  get 'mypage', to: 'users#me'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'boards#index'
-  # get 'boards', to: 'boards#index'
-  # get 'boards/new', to: 'boards#new'
-  # post 'boards', to: 'boards#create'
-  # get 'boards/:id', to: 'boards#show'
+
   root 'home#index'
+  resources :users, only: %i[new create]
   resources :boards
   resources :comments, only: %i[create destroy]
 end
